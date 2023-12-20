ce7d9a98c84888d0fbc7f5500f15eb29c3a54dea
Yan <96141422+Tkolin@users.noreply.github.com>
2023-12-20 08:24:30 +0700

    Update MainWindow.axaml.cs

--- miniAvaloniaApp/MainWindow.axaml.cs
+++ miniAvaloniaApp/MainWindow.axaml.cs
@@ -8,37 +8,45 @@ namespace miniAvaloniaApp;
 
 public partial class MainWindow : Window
 {
-    private List<Order> OrderListData { get; set; }
-    private List<Order> OrderListView { get; set; }
+// Списки для хранения и вывода данных
+    private List<Order> OrderListData { get; set; } // Хранения (Data)
+    private List<Order> OrderListView { get; set; } // Вывод (View)
+    
     public MainWindow()
     {
         InitializeComponent();
-        DownloadDataGrid();
+        DownloadDataGrid(); 
     }
+
+
+    // Метод обновления СПИСКА ДАННЫХ ИЗ БАЗЫ
     public void DownloadDataGrid()
     {
         OrderListData = DataBaseManager.GetOrders();
         UpdateDataGrid();
     }
 
+    // Метод отображения И фильтрации таблицы
     private void UpdateDataGrid()
     {
-        OrderListView = OrderListData; // Установка данных из базы в вид
+        OrderListView = OrderListData; // Установка данных ИЗ СПИСКА ХРАНЕНИЯ В СПИСОК ОТОБРАЖЕНИЯ
         if (SearchBox.Text.Length >= 1) // Фильтрация
             OrderListView = OrderListView.Where(c =>
-                c.ID.ToString().ToLower().Contains(SearchBox.Text.ToLower()) ||
-                c.Soderjanie.ToLower().ToString().Contains(SearchBox.Text.ToLower()) ||
+                c.ID.ToString().ToLower().Contains(SearchBox.Text.ToLower()) || // Поиск по ID
+                c.Soderjanie.ToLower().ToString().Contains(SearchBox.Text.ToLower()) || // Поиск по содержанию
                 c.Trebovania.ToLower().ToString().Contains(SearchBox.Text.ToLower()) ||
                 c.Name.ToLower().ToString().Contains(SearchBox.Text.ToLower()))
             .ToList();
-        DataGrid.ItemsSource = OrderListView;
+        DataGrid.ItemsSource = OrderListView;// Вывод таблицы
     }
 
+    // Событие ввода текста в поле поиска
     private void SearchBox_OnTextChanged(object? sender, TextChangedEventArgs e)
     {
         UpdateDataGrid();
     }
 
+    // Сброс поля поиска
     private void ResetBtn_OnClick(object? sender, RoutedEventArgs e)
     {
         SearchBox.Text = "";
